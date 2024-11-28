f0f2d7c544b69ae743acb7a0af8f2fadea5c600f
Olivier G <zotri@hotmail.com>
2024-11-28 17:26:25 +0100

    nvim maps: leader-space: telescope find file

--- dotfiles/config/nvim/lua/ocgg/plugins/telescope.lua
+++ dotfiles/config/nvim/lua/ocgg/plugins/telescope.lua
@@ -26,6 +26,8 @@ return {
 
 		-- [f]ind keymaps
 		keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+		-- duplicate for convenience
+		keymap.set("n", "<leader><space>", builtin.find_files, { desc = "Telescope find files" })
 		keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
 		keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
 		keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
