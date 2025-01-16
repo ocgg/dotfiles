ddaafed51494d31e1bec59475705d4516af3a0b1
Olivier G <zotri@hotmail.com>
2025-01-16 14:32:53 +0100

    copilot return

--- dotfiles/config/nvim/lua/ocgg/plugins/copilot.lua
+++ dotfiles/config/nvim/lua/ocgg/plugins/copilot.lua
@@ -1,37 +1,36 @@
 return {
-	-- 'zbirenbaum/copilot.lua',
-	-- cmd = 'Copilot',
-	-- event = 'InsertEnter',
-	-- config = function()
-	--   require('copilot').setup {
-	--     panel = {
-	--       enabled = true,
-	--       auto_refresh = false,
-	--       keymap = {
-	--         jump_prev = '[[',
-	--         jump_next = ']]',
-	--         accept = '<CR>',
-	--         refresh = 'gr',
-	--         open = '<M-CR>',
-	--       },
-	--       layout = {
-	--         position = 'right', -- bottom | top | left | right
-	--         ratio = 0.4,
-	--       },
-	--     },
-	--     suggestion = {
-	--       enabled = true,
-	--       auto_trigger = true,
-	--       debounce = 75,
-	--       keymap = {
-	--         accept = '<M-l>',
-	--         accept_word = false,
-	--         accept_line = false,
-	--         next = '<M-$>',
-	--         prev = '<M-^>',
-	--         dismiss = '<C-]>',
-	--       },
-	--     },
-	--   }
-	-- end,
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    event = 'InsertEnter',
+    opts = {
+        panel = {
+            enabled = true,
+            auto_refresh = false,
+            keymap = {
+                jump_prev = '[[',
+                jump_next = ']]',
+                accept = '<A-l>',
+                refresh = 'gr',
+                open = '<M-CR>',
+            },
+            layout = {
+                position = 'right',     -- bottom | top | left | right
+                ratio = 0.4,
+            },
+        },
+        suggestion = {
+            enabled = true,
+            auto_trigger = false,
+            hide_during_completion = true,
+            debounce = 200,
+            keymap = {
+                accept = '<A-l>',
+                accept_word = '<A-$>',
+                accept_line = false,
+                next = '<A-*>',
+                prev = '<A-ù>',
+                dismiss = '<A-w>',
+            },
+        },
+    }
 }
