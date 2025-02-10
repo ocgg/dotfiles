823438c049533be68e078e5b0f49c43f30a4f45d
ocgg <olivier.g@lavache.com>
2025-02-10 11:41:15 +0100

    tabstop & shiftwidth per filetype

--- dotfiles/config/nvim/lua/ocgg/autocommands.lua
+++ dotfiles/config/nvim/lua/ocgg/autocommands.lua
@@ -1,45 +1,40 @@
 -- :nohl after a search AND after the cursor moves
 -- from reddit (https://www.reddit.com/r/neovim/comments/1ct2w2h/lua_adaptation_of_vimcool_auto_nohlsearch/)
 vim.api.nvim_create_autocmd("CursorMoved", {
-	callback = function()
-		if vim.v.hlsearch == 1 and vim.fn.searchcount().exact_match == 0 then
-			vim.schedule(function()
-				vim.cmd.nohlsearch()
-			end)
-		end
-	end,
-	group = vim.api.nvim_create_augroup("auto-hlsearch", { clear = true }),
+    callback = function()
+        if vim.v.hlsearch == 1 and vim.fn.searchcount().exact_match == 0 then
+            vim.schedule(function()
+                vim.cmd.nohlsearch()
+            end)
+        end
+    end,
+    group = vim.api.nvim_create_augroup("auto-hlsearch", { clear = true }),
 })
 
 -- Highlight on yank (from kickstart)
 -- See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
-	callback = function()
-		vim.highlight.on_yank()
-	end,
-	group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
-	pattern = "*",
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+    group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
+    pattern = "*",
 })
 
 -- set wrap on markdown & text files
 vim.api.nvim_create_autocmd("FileType", {
-	callback = function()
-		if vim.bo.filetype == "markdown" or vim.bo.filetype == "text" then
-			vim.wo.wrap = true
-		end
-	end,
-	group = vim.api.nvim_create_augroup("WrapText", { clear = true }),
-	pattern = "*",
-})
-
--- set tabstop & shiftwidth for specific filetypes
-vim.api.nvim_create_autocmd("FileType", {
-	callback = function()
-		if vim.bo.filetype == "ruby" then
-			vim.bo.tabstop = 2
-			vim.bo.shiftwidth = 2
-		end
-	end,
-	group = vim.api.nvim_create_augroup("TabStopShiftWidth", { clear = true }),
-	pattern = "*",
+    callback = function()
+        local ftype = vim.bo.filetype
+        if ftype == "markdown" or ftype == "text" then
+            vim.wo.wrap = true
+        end
+        if ftype == "ruby"
+            or ftype == "markdown"
+            or ftype == "eruby" then
+            vim.bo.tabstop = 2
+            vim.bo.shiftwidth = 2
+        end
+    end,
+    group = vim.api.nvim_create_augroup("TabStopShiftWidthAndWrap", { clear = true }),
+    pattern = "*",
 })
