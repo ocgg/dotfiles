66de33d2216c0f2896151177e8b36ce6ff8ebd67
ocgg <olivier.g@lavache.com>
2025-02-10 11:44:00 +0100

    ruby_lsp path

--- dotfiles/config/nvim/lua/ocgg/plugins/lsp/lspconfig.lua
+++ dotfiles/config/nvim/lua/ocgg/plugins/lsp/lspconfig.lua
@@ -1,68 +1,68 @@
 return {
-	"neovim/nvim-lspconfig",
-	event = { "BufReadPre", "BufNewFile" },
-	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-		{ "folke/neodev.nvim", opts = {} },
-		{ "j-hui/fidget.nvim", opts = {} },
-	},
-	config = function()
-		local nvim_lsp = require("lspconfig")
-		local mason_lspconfig = require("mason-lspconfig")
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+        { "folke/neodev.nvim", opts = {} },
+        { "j-hui/fidget.nvim", opts = {} },
+    },
+    config = function()
+        local nvim_lsp = require("lspconfig")
+        local mason_lspconfig = require("mason-lspconfig")
 
-		-- local protocol = require("vim.lsp.protocol")
+        -- local protocol = require("vim.lsp.protocol")
 
-		local on_attach = function(client, bufnr)
-			-- format on save
-			-- if client.server_capabilities.documentFormattingProvider then
-			-- 	vim.api.nvim_create_autocmd("BufWritePre", {
-			-- 		group = vim.api.nvim_create_augroup("Format", { clear = true }),
-			-- 		buffer = bufnr,
-			-- 		callback = function()
-			-- 			vim.lsp.buf.format()
-			-- 		end,
-			-- 	})
-			-- end
-		end
+        -- local on_attach = function(client, bufnr)
+        -- format on save
+        -- if client.server_capabilities.documentFormattingProvider then
+        -- 	vim.api.nvim_create_autocmd("BufWritePre", {
+        -- 		group = vim.api.nvim_create_augroup("Format", { clear = true }),
+        -- 		buffer = bufnr,
+        -- 		callback = function()
+        -- 			vim.lsp.buf.format()
+        -- 		end,
+        -- 	})
+        -- end
+        -- end
 
-		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+        local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-		mason_lspconfig.setup_handlers({
-			-- The first entry (without a key) will be the default handler
-			-- and will be called for each installed server that doesn't have
-			-- a dedicated handler.
-			function(server)
-				nvim_lsp[server].setup({
-					on_attach = on_attach,
-					capabilities = capabilities,
-				})
-			end,
-			-- Next, you can provide a dedicated handler for specific servers.
-			-- ["ruby_lsp"] = function()
-			-- 	nvim_lsp.ruby_lsp.setup({
-			-- 		on_attach = on_attach,
-			-- 		capabilities = capabilities,
-			-- 		mason = false,
-			-- 		cmd = { "ruby-lsp" },
-			-- 	})
-			-- end,
-			-- ["rubocop"] = function()
-			-- 	nvim_lsp.rubocop.setup({
-			-- 		on_attach = on_attach,
-			-- 		capabilities = capabilities,
-			-- 		mason = false,
-			-- 		-- cmd = { vim.fn.systemlist("which rubocop")[1] },
-			-- 		cmd = { vim.fn.expand("~/.rbenv/shims/rubocop") },
-			-- 	})
-			-- end,
-			-- ["emmet_language_server"] = function()
-			-- 	nvim_lsp.emmet_language_server.setup({
-			-- 		on_attach = on_attach,
-			-- 		capabilities = capabilities,
-			-- 		-- mason = false,
-			-- 		-- cmd = { vim.fn.systemlist("which emmet-language-server")[1] },
-			-- 	})
-			-- end,
-		})
-	end,
+        mason_lspconfig.setup_handlers({
+            -- The first entry (without a key) will be the default handler
+            -- and will be called for each installed server that doesn't have
+            -- a dedicated handler.
+            function(server)
+                nvim_lsp[server].setup({
+                    -- on_attach = on_attach,
+                    capabilities = capabilities,
+                })
+            end,
+            -- Next, you can provide a dedicated handler for specific servers.
+            ["ruby_lsp"] = function()
+                nvim_lsp.ruby_lsp.setup({
+                    -- on_attach = on_attach,
+                    capabilities = capabilities,
+                    -- mason = false,
+                    cmd = { vim.fn.expand "~/.rbenv/shims/ruby-lsp" },
+                })
+            end,
+            -- ["stimulus_ls"] = function()
+            --     nvim_lsp.stimulus_ls.setup({
+            --         -- on_attach = on_attach,
+            --         capabilities = capabilities,
+            --         -- mason = false,
+            --         -- cmd = { vim.fn.expand "~/.rbenv/shims/ruby-lsp" },
+            --         filetypes = { "eruby" }
+            --     })
+            -- end,
+            -- ["emmet_language_server"] = function()
+            -- 	nvim_lsp.emmet_language_server.setup({
+            -- 		on_attach = on_attach,
+            -- 		capabilities = capabilities,
+            -- 		-- mason = false,
+            -- 		-- cmd = { vim.fn.systemlist("which emmet-language-server")[1] },
+            -- 	})
+            -- end,
+        })
+    end,
 }
