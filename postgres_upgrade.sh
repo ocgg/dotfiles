87a381c2314b888d13b47c9f31f031f9dd97821e
ocgg <olivier.g@lavache.com>
2025-08-17 23:07:12 +0200

    refresh collation version as postgres user

--- postgres_upgrade.sh
+++ postgres_upgrade.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Liste de toutes les bases de données autorisées
-DB_LIST=$(psql -Atc "SELECT datname FROM pg_database WHERE datallowconn = true;")
+DB_LIST=$(psql -U postgres -Atc "SELECT datname FROM pg_database WHERE datallowconn = true;")
 
 for db in $DB_LIST; do
     echo "Traitement de $db"
     
     # Réindexer la base
-    psql -c "REINDEX DATABASE $db;" "$db"
+    psql -U postgres -c "REINDEX DATABASE $db;" "$db"
     
     # Rafraîchir la version de collation
-    psql -c "ALTER DATABASE $db REFRESH COLLATION VERSION;" "$db"
+    psql -U postgres -c "ALTER DATABASE $db REFRESH COLLATION VERSION;" "$db"
     
     echo "$db terminé."
 done
